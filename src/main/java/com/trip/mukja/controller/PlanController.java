8a24d4777b0d5a2eda23f188fbe3bf28bc0e8657
홍승준 <ghdgn22@naver.com>
2023-05-12 17:42:54 +0900

    [update] 플ë플플래너 ê³작성 기능

--- src/main/java/com/trip/mukja/controller/PlanController.java
+++ src/main/java/com/trip/mukja/controller/PlanController.java
@@ -1,33 +1,44 @@
 package com.trip.mukja.controller;
 
+import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.trip.mukja.model.dto.PlanDTO;
 import com.trip.mukja.service.PlanService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 @RestController
 @RequestMapping("/plans")
+@CrossOrigin("*")
+@Api(tags = {"여행 계획"})
+@Slf4j
 public class PlanController {
+    private final Logger logger = LoggerFactory.getLogger(PlanController.class);
+    private PlanService planService;
 
-	private PlanService planService ;
-	
-	public PlanController(PlanService planService) {
-		super();
-		this.planService = planService;
-	}
+    public PlanController(PlanService planService) {
+        super();
+        this.planService = planService;
+    }
 
-	// 여행계획
-	@PostMapping()
-	public ResponseEntity<?> makePlanner(@RequestBody PlanDTO planDTO){
-//		List<LocalDate> days = planService.get
-		return null;
-	}
-	
+
+    // 사용자가 입력한 여행 계획 정보를 받아 플래너를 만들어준다.
+
+    @ApiOperation(value = "여행 플래너 작성", notes = "여행 플래너 작성 API")
+    @PostMapping("/")
+    public ResponseEntity<?> makePlanner(@RequestBody PlanDTO planDTO) {
+        logger.debug("test"+planDTO.toString());
+        // 플래너를 생성한다.
+        planService.makePlanner(planDTO);
+
+        // 시작과 끝 날짜를 계산하여 값을 넘겨준다.
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/trip/mukja/model/dto/PlanDTO.java
+++ src/main/java/com/trip/mukja/model/dto/PlanDTO.java
@@ -1,15 +1,20 @@
 package com.trip.mukja.model.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Getter
 @Setter
 @ToString
+@ApiModel(value="PlanDTO (여행 플래너 정보)")
 public class PlanDTO {
 
 	private int planId;
@@ -18,12 +23,11 @@ public class PlanDTO {
 	private String content;
 	private int hit;
 	private int like;
-	private LocalDate fDate;
-	private LocalDate lDate;
-	private LocalDate wDate;
-	
-	private List<LocalDate> days;
-	
+	private LocalDateTime fDate;
+	private LocalDateTime lDate;
+	private LocalDateTime wDate;
+
+
 	
 
 }
