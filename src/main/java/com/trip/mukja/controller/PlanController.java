79fe6c2a94948df455494a7a34315830dc3f439a
Seungjun <ghdgn22@naver.com>
2023-05-14 22:05:30 +0900

    [modify] 플래너 작성시 인가 여부 확인

--- src/main/java/com/trip/mukja/controller/PlanController.java
+++ src/main/java/com/trip/mukja/controller/PlanController.java
@@ -4,8 +4,10 @@ import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import com.trip.mukja.model.dto.PlanDTO;
@@ -29,15 +31,17 @@ public class PlanController {
     // 사용자가 입력한 여행 계획 정보를 받아 플래너를 만들어준다.
 
     @ApiOperation(value = "여행 플래너 작성", notes = "여행 플래너 작성 API")
-    @PostMapping("/")
-    public ResponseEntity<?> makePlanner(@RequestBody PlanDTO planDTO) {
+    @PostMapping
+    public ResponseEntity<?> makePlanner(@RequestBody PlanDTO planDTO, Authentication authentication) {
+
+
         logger.debug("test"+planDTO.toString());
         // 플래너를 생성한다.
         planService.makePlanner(planDTO);
 
         // 시작과 끝 날짜를 계산하여 값을 넘겨준다.
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().body(authentication.getName()+"님의 등록이 완료 되었습니다.");
     }
 
 
