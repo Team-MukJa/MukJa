248a1f0b4e36c5a9a3da02ef4562563988dde177
변영채 <baeyonyk124@gmail.com>
2023-05-18 15:58:01 +0900

    [feat|update|delete] 관광지 및 리뷰 관련 백 기능 추가

---
+++ src/main/java/com/trip/mukja/model/dto/ReviewDTO.java
@@ -0,0 +1,23 @@
+package com.trip.mukja.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@ApiModel(value="ReviewDTO (리뷰 정보)")
+public class ReviewDTO {
+
+	private int reviewId;
+	private int placeId;
+	private String userId;
+	private String content;
+	private int rating;
+	private String registerTime;
+
+}
